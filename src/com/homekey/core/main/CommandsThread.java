83c9521bd41d16e0a5bb950facef641bd5a87f09
Linus <linus@tufve.com>
2011-07-26 17:13:40 +0200

    okej

--- src/com/homekey/core/main/CommandsThread.java
+++ src/com/homekey/core/main/CommandsThread.java
@@ -1,5 +1,30 @@
 package com.homekey.core.main;
 
-public class CommandsThread {
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class CommandsThread extends Thread {
+	BlockingQueue<Runnable> workQueue;
+
+	@Override
+	public void run() {
+		workQueue = new LinkedBlockingQueue<Runnable>();
+		while (true) {
+			Runnable r = null;
+			try {
+				r = workQueue.take();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			if (r != null) {
+				r.run();
+			}
+		}
+	}
+
+	public void post(Runnable r) {
+		workQueue.offer(r);
+	}
 
 }
