4b0c581bcc1edd6dde50338c91e63895eb7032a8
Marcus Johansson <m@rcuz.se>
2011-07-26 18:17:50 +0200

    Fixed bugg caused by bad merge.

--- src/com/homekey/core/device/onewire/OneWireTemperatureSensor.java
+++ src/com/homekey/core/device/onewire/OneWireTemperatureSensor.java
@@ -4,5 +4,7 @@ import com.homekey.core.device.Device;
 import com.homekey.core.device.IntervalLoggable;
 
 public class OneWireTemperatureSensor extends Device implements IntervalLoggable {
-	
+	public OneWireTemperatureSensor(int id, String name) {
+		super(id, name);
+	}
 }
