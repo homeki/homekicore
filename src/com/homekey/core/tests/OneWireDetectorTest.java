69ca689a889063714e93ddc1c395fbe25562e76c
Jonas Oscarsson <jonas.osc@rsson.nu>
2011-07-30 18:02:44 +0200

    Added 1-wire test sensor structure (copied from live 1-wire structure).

---
+++ src/com/homekey/core/tests/OneWireDetectorTest.java
@@ -0,0 +1,5 @@
+package com.homekey.core.tests;
+
+public class OneWireDetectorTest {
+	
+}
--- src/com/homekey/core/tests/TellStickSwitchTest.java
+++ src/com/homekey/core/tests/TellStickSwitchTest.java
@@ -10,6 +10,8 @@ public class TellStickSwitchTest {
 	private static final String INTERNAL_ID = "mock1";
 	private static final String NAME = "My Device #5";
 	
+	// TODO: Linus: implement more testing
+	
 	private TellStickSwitch device;
 	private Database db;
 	
