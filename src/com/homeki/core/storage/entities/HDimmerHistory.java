267e17f59b077b13991ec205c0a022ea802943d7
Jonas Oscarsson <jonas.osc@rsson.nu>
2011-12-18 13:34:26 +0100

    Added hbm files for other history tables.

--- src/com/homeki/core/storage/entities/HDimmerHistory.java
+++ src/com/homeki/core/storage/entities/HDimmerHistory.java
@@ -1,5 +1,38 @@
 package com.homeki.core.storage.entities;
 
+import java.util.Date;
+
 public class HDimmerHistory {
+	private Integer id;
+	private HDevice device;
+	private Date registered;
+	private Integer value;
 	
+	public Integer getId() {
+		return id;
+	}
+	
+	public HDevice getDevice() {
+		return device;
+	}
+
+	public void setDevice(HDevice device) {
+		this.device = device;
+	}
+	
+	public Date getRegistered() {
+		return registered;
+	}
+	
+	public void setRegistered(Date registered) {
+		this.registered = registered;
+	}
+	
+	public Integer getValue() {
+		return value;
+	}
+	
+	public void setValue(Integer value) {
+		this.value = value;
+	}
 }
--- src/com/homeki/core/storage/entities/HTemperatureHistory.java
+++ src/com/homeki/core/storage/entities/HTemperatureHistory.java
@@ -1,5 +1,38 @@
 package com.homeki.core.storage.entities;
 
+import java.util.Date;
+
 public class HTemperatureHistory {
+	private Integer id;
+	private HDevice device;
+	private Date registered;
+	private Float value;
 	
+	public Integer getId() {
+		return id;
+	}
+	
+	public HDevice getDevice() {
+		return device;
+	}
+
+	public void setDevice(HDevice device) {
+		this.device = device;
+	}
+	
+	public Date getRegistered() {
+		return registered;
+	}
+	
+	public void setRegistered(Date registered) {
+		this.registered = registered;
+	}
+	
+	public Float getValue() {
+		return value;
+	}
+	
+	public void setValue(Float value) {
+		this.value = value;
+	}
 }
