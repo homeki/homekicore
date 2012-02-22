5ab37d345b01bfa00ad70890d5f707c1c18e693a
Jonas Oscarsson <jonas.osc@rsson.nu>
2012-02-22 22:44:23 +0100

    Made intervals more human readable.

--- src/com/homeki/core/main/Configuration.java
+++ src/com/homeki/core/main/Configuration.java
@@ -3,8 +3,7 @@ package com.homeki.core.main;
 public class Configuration {
 	public final static boolean MOCK_ENABLED = false;
 	public final static int TIMER_THREAD_INTERVAL = 10*1000;
-	public final static int TELLSTICK_DETECTOR_INTERVAL = 120*1000;
-	public final static int ONEWIRE_DETECTOR_INTERVAL = 120*1000;
-	public final static int ONEWIRE_COLLECTOR_INTERVAL = 900*1000;
+	public final static int ONEWIRE_DETECTOR_INTERVAL = 2*60*1000;
+	public final static int ONEWIRE_COLLECTOR_INTERVAL = 15*60*1000;
 	public final static String ONEWIRE_PATH = "/mnt/1wire";
 }
