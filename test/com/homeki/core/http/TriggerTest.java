9161014c52f0daa5b2e9181024965515f26f0a58
Jonas Oscarsson <jonas.osc@rsson.nu>
2012-08-24 23:14:35 +0200

    Added test for list and delete for triggers.

--- test/com/homeki/core/http/TriggerTest.java
+++ test/com/homeki/core/http/TriggerTest.java
@@ -1,11 +1,18 @@
 package com.homeki.core.http;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import com.homeki.core.TestUtil;
 
 public class TriggerTest {
+	private int triggerId;
+	
 	public class JsonTrigger {
 		public Integer id;
 		public String name;
@@ -20,7 +27,23 @@ public class TriggerTest {
 		jtrigger.name = "MyTrigger";
 		jtrigger = TestUtil.sendPostAndParseAsJson("/trigger/add", jtrigger, JsonTrigger.class);
 		assertTrue(jtrigger.id > 0);
+		triggerId = jtrigger.id;
 	}
 	
-	// TODO: add delete
+	@Test(dependsOnMethods="testAdd")
+	public void testList() {
+		JsonTrigger[] jtriggers = TestUtil.sendGetAndParseAsJson("/trigger/list", JsonTrigger[].class);
+		
+		Set<Integer> existingIds = new HashSet<Integer>();
+		
+		for (JsonTrigger jd : jtriggers)
+			existingIds.add(jd.id);
+		
+		assertTrue(existingIds.contains(triggerId));
+	}
+	
+	@Test(dependsOnMethods="testList")
+	public void testDelete() {
+		assertEquals(TestUtil.sendGet("/trigger/" + triggerId + "/delete").statusCode, 200);
+	}
 }
