b8d8e32a5846da0494abc9dbadd5f841aff739de
Jonas Oscarsson <jonas.osc@rsson.nu>
2013-12-02 00:13:15 +0100

    Sets password for postgres user in postgresql and enables client connections

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -32,11 +32,16 @@ apt-get update
 # From standard repositories
 apt-get install -y openjdk-6-jre-headless postgresql-9.1 libjna-java libgnumail-java vim
 
+# Set db su user password and enable client connections
+echo "ALTER USER postgres WITH PASSWORD 'password';" | sudo -u postgres psql
+echo "host all all 0.0.0.0/0 md5" >> /etc/postgresql/9.1/main/pg_hba.conf
+sed -i -e "s/\#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.1/main/postgresql.conf
+/etc/init.d/postgresql restart
+
 # From homeki repository
 apt-get install -y telldus owfs
 
 # Create database and database user
 echo "CREATE USER homeki WITH PASSWORD 'homeki';" | sudo -u postgres psql
 echo "CREATE DATABASE homeki WITH owner = homeki;" | sudo -u postgres psql
-
 
