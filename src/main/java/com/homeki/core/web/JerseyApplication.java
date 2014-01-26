962a8c5f930e0298471a4d1dbe3e5b14964908e3
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-01-26 15:21:56 +0100

    Adds CORS filter, allow any origin

--- src/main/java/com/homeki/core/web/JerseyApplication.java
+++ src/main/java/com/homeki/core/web/JerseyApplication.java
@@ -2,11 +2,13 @@ package com.homeki.core.web;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import com.homeki.core.web.filters.CharsetResponseFilter;
+import com.homeki.core.web.filters.CrossOriginResourceSharingResponseFilter;
 import com.homeki.core.web.filters.LogRequestFilter;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class JerseyApplication extends ResourceConfig {
 	public JerseyApplication() {
+		register(CrossOriginResourceSharingResponseFilter.class);
 		register(LogRequestFilter.class);
 		register(CharsetResponseFilter.class);
 		register(JacksonContextResolver.class);
---
+++ src/main/java/com/homeki/core/web/filters/CrossOriginResourceSharingResponseFilter.java
@@ -0,0 +1,13 @@
+package com.homeki.core.web.filters;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import java.io.IOException;
+
+public class CrossOriginResourceSharingResponseFilter implements ContainerResponseFilter {
+	@Override
+	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+		responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+	}
+}
