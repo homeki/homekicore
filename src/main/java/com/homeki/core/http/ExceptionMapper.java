febb717f210c66b94ffc6783467096f75d659e08
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-02-13 23:05:09 +0100

    Improves API output when JSON mapping exception and any exception occur

--- src/main/java/com/homeki/core/http/ExceptionMapper.java
+++ src/main/java/com/homeki/core/http/ExceptionMapper.java
@@ -1,5 +1,6 @@
 package com.homeki.core.http;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.homeki.core.json.JsonVoid;
 import com.homeki.core.logging.L;
 
@@ -13,15 +14,16 @@ public class ExceptionMapper implements javax.ws.rs.ext.ExceptionMapper<Throwabl
 		} catch (ApiException e) {
 			L.w("API exception occurred: " + e.getMessage());
 			return Response.status(400).entity(new JsonVoid(e.getMessage())).build();
+		} catch (JsonMappingException e) {
+			L.w("Failed to map JSON object: " + e.getMessage());
+			return Response.status(400).entity(new JsonVoid("Failed to to construct object from JSON.")).build();
 		} catch (Throwable e) {
 			L.e("Unknown exception occurred while processing request.", e);
-
 			String exceptionType = e.getClass().getSimpleName();
-
 			if (e.getMessage() != null) {
-				return Response.status(400).entity("Unhandled " + exceptionType + " occurred while processing request: " + e.getMessage()).build();
+				return Response.status(400).entity(new JsonVoid("Unhandled " + exceptionType + " occurred while processing request: " + e.getMessage())).build();
 			} else {
-				return Response.status(400).entity("Unhandled " + exceptionType + " occurred while processing request.").build();
+				return Response.status(400).entity(new JsonVoid("Unhandled " + exceptionType + " occurred while processing request.")).build();
 			}
 		}
 	}
