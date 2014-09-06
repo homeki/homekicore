1c3035eac176cdfe7ebab85ecea81998628a30f5
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-09-07 01:04:48 +0200

    Add autodeploy to S3 debian repo

---
+++ script/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script deploys to S3 debian repository.
+
+DEB_CODENAME=$1
+
+./gradlew dist
+mv build/dist/homeki_*_all.deb build/dist/homeki.deb
+deb-s3 upload --endpoint s3-eu-west-1.amazonaws.com --access-key-id=$S3_ACCESS_KEY --secret-access-key=$S3_ACCESS_SECRET --codename $DEB_CODENAME --bucket repository.homeki.com build/dist/homeki.deb
