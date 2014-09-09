2fe1452216004a268bc9b79ffa8c3fa4f0107947
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-09-09 23:27:25 +0200

    Modify circleci, install deb-s3 on each deploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,6 +4,7 @@
 
 DEB_CODENAME=$1
 
+gem install deb-s3
 ./gradlew dist
 mv build/dist/homeki_*_all.deb build/dist/homeki.deb
 deb-s3 upload --endpoint s3-eu-west-1.amazonaws.com --access-key-id=$S3_ACCESS_KEY --secret-access-key=$S3_ACCESS_SECRET --codename $DEB_CODENAME --bucket repository.homeki.com build/dist/homeki.deb
--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -7,6 +7,5 @@ DEPS="$BUILD_META_HOME/installed-dependencies"
 if [ ! -e $DEPS ]; then
   mkdir $BUILD_META_HOME &&
   ./gradlew tasks &&
-  gem install deb-s3 &&
   touch $DEPS
 fi
