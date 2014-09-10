3abf7ed5231b38b073c648874a27829fad0290e4
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-09-10 22:25:32 +0200

    Use bundle exec to run deb-s3

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -7,6 +7,9 @@ DEPS="$BUILD_META_HOME/installed-dependencies"
 if [ ! -e $DEPS ]; then
   mkdir $BUILD_META_HOME &&
   ./gradlew tasks &&
-  pushd script > /dev/null && bundle install --path="$BUILD_META_HOME/bundle" && popd > /dev/null &&
   touch $DEPS
 fi
+
+# always run as bundle install will not reinstall and we need to let bundler know
+# the custom --path
+pushd script > /dev/null && bundle install --path="$BUILD_META_HOME/bundle" && popd > /dev/null
