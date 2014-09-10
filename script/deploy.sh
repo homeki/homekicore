a30f7786437dbde8ab9f10853f9e7d77026bd611
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-09-10 20:07:17 +0200

    Revert to use homeki.deb as pkg name
    deb-s3 does not delete old .debs

--- script/deploy.sh
+++ script/deploy.sh
@@ -14,5 +14,5 @@ gpg --import homeki-private-gpg.asc
 rm homeki-private-gpg.asc
 set -e
 gem install deb-s3
-DEB_NAME=`ls build/dist/`
-deb-s3 upload --endpoint s3-eu-west-1.amazonaws.com --sign $GPG_KEY_ID --access-key-id=$S3_ACCESS_KEY --secret-access-key=$S3_ACCESS_SECRET --codename $DEB_CODENAME --prefix packages --bucket repository.homeki.com build/dist/$DEB_NAME
+mv build/dist/homeki_*_all.deb build/dist/homeki.deb
+deb-s3 upload --endpoint s3-eu-west-1.amazonaws.com --sign $GPG_KEY_ID --access-key-id=$S3_ACCESS_KEY --secret-access-key=$S3_ACCESS_SECRET --codename $DEB_CODENAME --prefix packages --bucket repository.homeki.com build/dist/homeki.deb
