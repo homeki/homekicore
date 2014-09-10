5ef6ae39ac8f423dd1f9782916afd31f0f96a113
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-09-10 20:21:42 +0200

    Try to cache deb-s3

--- script/deploy.sh
+++ script/deploy.sh
@@ -15,4 +15,4 @@ rm homeki-private-gpg.asc
 set -e
 gem install deb-s3
 mv build/dist/homeki_*_all.deb build/dist/homeki.deb
-deb-s3 upload --endpoint s3-eu-west-1.amazonaws.com --sign $GPG_KEY_ID --access-key-id=$S3_ACCESS_KEY --secret-access-key=$S3_ACCESS_SECRET --codename $DEB_CODENAME --prefix packages --bucket repository.homeki.com build/dist/homeki.deb
+$BUILD_META_HOME/bundle/deb-s3 upload --endpoint s3-eu-west-1.amazonaws.com --sign $GPG_KEY_ID --access-key-id=$S3_ACCESS_KEY --secret-access-key=$S3_ACCESS_SECRET --codename $DEB_CODENAME --prefix packages --bucket repository.homeki.com build/dist/homeki.deb
--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -7,5 +7,6 @@ DEPS="$BUILD_META_HOME/installed-dependencies"
 if [ ! -e $DEPS ]; then
   mkdir $BUILD_META_HOME &&
   ./gradlew tasks &&
+  pushd script > /dev/null && bundle install --path="$BUILD_META_HOME/bundle" && popd > /dev/null &&
   touch $DEPS
 fi
