39d9c90d562df0086615e09aeee15271ecdc3df5
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-09-10 16:57:40 +0200

    Fix deb name for release

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,11 +2,17 @@
 
 # This script deploys to S3 debian repository.
 
+set -e
+
 DEB_CODENAME=$1
 
-echo $GPG_PRIVATE_KEY | base64 --decode > homeki-private-gpg.asc
-gpg --import homeki-private-gpg.asc
-gem install deb-s3
 ./gradlew dist
-mv build/dist/homeki_*_all.deb build/dist/homeki.deb
-deb-s3 upload --endpoint s3-eu-west-1.amazonaws.com --sign $GPG_KEY_ID --access-key-id=$S3_ACCESS_KEY --secret-access-key=$S3_ACCESS_SECRET --codename $DEB_CODENAME --prefix packages --bucket repository.homeki.com build/dist/homeki.deb
+
+echo $GPG_PRIVATE_KEY | base64 --decode > homeki-private-gpg.asc
+set +e
+gpg --import homeki-private-gpg.asc
+rm homeki-private-gpg.asc
+set -e
+gem install deb-s3
+DEB_NAME=`ls build/dist/`
+deb-s3 upload --endpoint s3-eu-west-1.amazonaws.com --sign $GPG_KEY_ID --access-key-id=$S3_ACCESS_KEY --secret-access-key=$S3_ACCESS_SECRET --codename $DEB_CODENAME --prefix packages --bucket repository.homeki.com build/dist/$DEB_NAME
