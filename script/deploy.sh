00071d691c52d63cd3426794a0705bedc8aaee5c
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-11-16 21:37:01 +0100

    Separate deb for stable and unstable

--- script/deploy.sh
+++ script/deploy.sh
@@ -13,7 +13,7 @@ set +e
 gpg --import homeki-private-gpg.asc
 rm homeki-private-gpg.asc
 set -e
-mv build/dist/homeki_*_all.deb build/dist/homeki.deb
+mv build/dist/homeki_*_all.deb build/dist/homeki_${DEB_CODENAME}.deb
 pushd script > /dev/null
-bundle exec deb-s3 upload --endpoint s3-eu-west-1.amazonaws.com --sign $GPG_KEY_ID --access-key-id=$S3_ACCESS_KEY --secret-access-key=$S3_ACCESS_SECRET --codename $DEB_CODENAME --prefix packages --bucket repository.homeki.com ../build/dist/homeki.deb
+bundle exec deb-s3 upload --endpoint s3-eu-west-1.amazonaws.com --sign $GPG_KEY_ID --access-key-id=$S3_ACCESS_KEY --secret-access-key=$S3_ACCESS_SECRET --codename $DEB_CODENAME --prefix packages --bucket repository.homeki.com ../build/dist/homeki_${DEB_CODENAME}.deb
 popd > /dev/null
