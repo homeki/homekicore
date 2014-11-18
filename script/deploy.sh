5b852c47e743a11c8117d9d7e75fa6b36c54b52c
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-11-18 23:12:40 +0100

    Add origin to deb upload

--- script/deploy.sh
+++ script/deploy.sh
@@ -15,5 +15,5 @@ rm homeki-private-gpg.asc
 set -e
 mv build/dist/homeki_*_all.deb build/dist/homeki_${DEB_CODENAME}.deb
 pushd script > /dev/null
-bundle exec deb-s3 upload --endpoint s3-eu-west-1.amazonaws.com --sign $GPG_KEY_ID --access-key-id=$S3_ACCESS_KEY --secret-access-key=$S3_ACCESS_SECRET --codename $DEB_CODENAME --prefix packages --bucket repository.homeki.com ../build/dist/homeki_${DEB_CODENAME}.deb
+bundle exec deb-s3 upload --endpoint s3-eu-west-1.amazonaws.com --sign $GPG_KEY_ID --access-key-id $S3_ACCESS_KEY --secret-access-key $S3_ACCESS_SECRET --codename $DEB_CODENAME --prefix packages --origin homeki.com --bucket repository.homeki.com ../build/dist/homeki_${DEB_CODENAME}.deb
 popd > /dev/null
